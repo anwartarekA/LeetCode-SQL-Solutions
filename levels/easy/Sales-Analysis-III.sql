8c03a9b4fdb6dae25d9a511ca793e2886399932f
Anwar Tarek <111369375+anwartarekA@users.noreply.github.com>
2026-04-03 02:45:31 +0100

    Add SQL query for sales analysis in Q1 2019

---
+++ levels/easy/Sales-Analysis-III.sql
@@ -0,0 +1,1 @@
+select Product.product_id , Product.product_name from Product inner join Sales on Product.product_id = Sales.product_id group by Product.product_id , Product.product_name having min(sale_date) >= '2019-01-01' AND max(sale_date) <= '2019-03-31';
