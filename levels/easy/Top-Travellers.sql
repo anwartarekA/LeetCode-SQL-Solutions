5f5f29915c9afb4f1ff4f1a0f42e3fa1c236247b
Anwar Tarek <111369375+anwartarekA@users.noreply.github.com>
2026-04-03 02:55:00 +0100

    Add SQL query to retrieve top travellers by distance

---
+++ levels/easy/Top-Travellers.sql
@@ -0,0 +1,1 @@
+select name , COALESCE(sum(Rides.distance),0) as travelled_distance from Users left outer join Rides on Users.id = Rides.user_id group by name,Users.id order by travelled_distance DESC, name ASC
